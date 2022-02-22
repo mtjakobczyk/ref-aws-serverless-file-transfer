9a74602108383686b220b38c5884c002b9dfb247
mtjakobczyk <mtjakobczyk@gmail.com>
2022-02-22 15:47:08 +0100

    DynamoDB Update

--- applications/fileTransferRequester/src/main/java/io/github/mtjakobczyk/references/aws/serverless/FileTransferRequester.java
+++ applications/fileTransferRequester/src/main/java/io/github/mtjakobczyk/references/aws/serverless/FileTransferRequester.java
@@ -1,14 +1,18 @@
 package io.github.mtjakobczyk.references.aws.serverless;
 
-import java.util.Map;
 import static java.util.Map.entry;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -21,7 +25,7 @@ public class FileTransferRequester implements RequestHandler<S3Event, String>{
   private Gson gson = new GsonBuilder().setPrettyPrinting().create();
   private Table fileTransfersTable;
   private LambdaLogger logger;
-  String fileProcessingId;
+  private String fileProcessingId;
   @Override
   public String handleRequest(S3Event event, Context context) {
     try {
@@ -33,22 +37,25 @@ public class FileTransferRequester implements RequestHandler<S3Event, String>{
         RecordFileTransferEventType.PUT,
         fileProcessingId, 
         Map.ofEntries(
-          entry("startOfProcessing", LocalTime.now().toString()), // ISO-8601 Format
+          entry("startOfProcessing", LocalDateTime.now().toString()), // ISO-8601 Format
           entry("filename", s3Event.getObject().getUrlDecodedKey()),
           entry("fileSizeBytes", s3Event.getObject().getSizeAsLong()),
           entry("fileVersion", s3Event.getObject().getVersionId())
         )
       );
 
+      // TODO Read File and Call API Gateway
 
-      // recordFileTransferEvent(
-      //   RecordFileTransferEventType.UPDATE,
-      //   fileProcessingId, 
-      //   Map.ofEntries(
-      //     entry("endOfProcessing", LocalTime.now().toString()), // ISO-8601 Format
-      //     entry("fileTransferId", null)
-      //   )
-      // );
+      var fileTransferId = UUID.randomUUID().toString();
+
+      recordFileTransferEvent(
+        RecordFileTransferEventType.UPDATE,
+        fileProcessingId, 
+        Map.ofEntries(
+          entry("endOfProcessing", LocalDateTime.now().toString()), // ISO-8601 Format
+          entry("fileTransferId", fileTransferId)
+        )
+      );
 
     } catch(Exception ex) {
       logger.log("ERROR " + fileProcessingId + " " + ex.getMessage());
@@ -62,7 +69,7 @@ public class FileTransferRequester implements RequestHandler<S3Event, String>{
     fileProcessingId = context.getAwsRequestId();
     logger.log("EVENT " + fileProcessingId + " " + gson.toJson(event));
     
-    var fileTransfersTableName = System.getenv("FILE_TRANSFERS_TABLE");
+    var fileTransfersTableName = getEnvironmentVariable("FILE_TRANSFERS_TABLE");
     if(fileTransfersTableName == null || fileTransfersTableName.isEmpty()) {
       throw new IllegalArgumentException("FILE_TRANSFERS_TABLE environment variables is not set");
     }
@@ -73,16 +80,36 @@ public class FileTransferRequester implements RequestHandler<S3Event, String>{
   }
 
   private void recordFileTransferEvent(RecordFileTransferEventType type, String fileProcessingId, Map<String, Object> attributes) {
-    Item item = Item.fromMap(attributes).withPrimaryKey("fileProcessingId", fileProcessingId);
-    logger.log("ITEM " + fileProcessingId + " " + item.toJSONPretty());
+    if(attributes == null || attributes.isEmpty()) {
+      throw new IllegalArgumentException("There are no attributes passed to the recordFileTransferEvent");
+    }
     switch(type) {
       case PUT:
+        var item = Item.fromMap(attributes).withPrimaryKey("fileProcessingId", fileProcessingId);
+        logger.log("PUTITEM " + fileProcessingId + " " + item.toJSONPretty());
         fileTransfersTable.putItem(item);
         break;
       case UPDATE:
-        //fileTransfersTable.updateItem()
+        StringBuilder updateExpressionBuilder = new StringBuilder("SET ");
+        var valueMapForUpdate = new ValueMap();
+        int i = 1;
+        for(Entry<String, Object> entry : attributes.entrySet()) {
+          String valueMapKey = ":v"+i++;
+          updateExpressionBuilder.append(entry.getKey()).append(" = ").append(valueMapKey).append(",");
+          valueMapForUpdate = valueMapForUpdate.withString(valueMapKey, entry.getValue().toString());
+        }
+        updateExpressionBuilder.deleteCharAt(updateExpressionBuilder.length()-1);
+        String updateExpression = updateExpressionBuilder.toString();
+        var updateItemSpec = new UpdateItemSpec().withPrimaryKey("fileProcessingId", fileProcessingId)
+                                    .withUpdateExpression(updateExpression).withValueMap(valueMapForUpdate);
+                                          
+        logger.log("UPDATEITEM " + fileProcessingId);
+        fileTransfersTable.updateItem(updateItemSpec);
         break;
     }
-    
+  }
+
+  protected String getEnvironmentVariable(String name) {
+    return System.getenv(name);
   }
 }
